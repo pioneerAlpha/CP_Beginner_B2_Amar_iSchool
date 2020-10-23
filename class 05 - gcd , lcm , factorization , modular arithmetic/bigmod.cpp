6f8616ecf9ec76323a209fdba4d6f2f824de2983
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-10-23 15:35:07 +0600

    Create bigmod.cpp

---
+++ class 05 - gcd , lcm , factorization , modular arithmetic/bigmod.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define N ((int)1e5 + 5)
+#define MOD ((int)1e9 + 7)
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MAX ((int)1e6 + 5)
+#define endl "\n"
+
+using namespace std;
+
+int power(int n , int p)
+{
+    int ans = power(n,p>>1);
+    ans = 1LL*ans*ans%MOD;
+    if(p&1) ans = 1LL*ans*n%MOD;
+    return ans;
+}
+
+int main()
+{
+    fastio;
+    while(1){
+        int n , p;
+        cin>>n>>p;
+        cout<<power(n%MOD,p)<<endl;
+    }
+    return 0;
+}

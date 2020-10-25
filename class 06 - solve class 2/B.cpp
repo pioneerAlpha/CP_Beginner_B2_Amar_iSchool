7e74a94440a08f1755f684803bbfd668ec27bf76
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-10-26 00:36:14 +0600

    Create B.cpp

---
+++ class 06 - solve class 2/B.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define N ((int)1e6 + 5)
+#define MOD ((int)1e9 + 7)
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MAX ((int)1e6 + 5)
+#define endl "\n"
+
+using namespace std;
+
+
+
+int main()
+{
+fastio;
+    int t;
+    t = 1;
+    while(t--){
+        string a , b;
+        cin>>a>>b;
+        if(a == b) cout<<a<<endl;
+        else cout<<"1\n";
+    }
+    return 0;
+}

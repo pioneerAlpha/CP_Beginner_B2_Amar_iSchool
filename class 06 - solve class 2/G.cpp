e322e8f81a09d706bc04f10df6aa05efd9fe79d9
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-10-26 00:39:58 +0600

    Create G.cpp

---
+++ class 06 - solve class 2/G.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define N ((int)1e6 + 5)
+#define MOD ((int)1e9 + 7)
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MAX ((int)1e6 + 5)
+#define endl "\n"
+
+using namespace std;
+
+
+
+int main()
+{
+    fastio;
+    int t;
+    cin>>t;
+    while(t--){
+         int n , k;
+         cin>>n>>k;
+         ll ans = 1LL*n*k/(n-1);
+         if(ans % n == 0 ) ans--;
+         cout<<ans<<endl;
+    }
+    return 0;
+}

72e71c8bf0b5b0bedcd26120daa5992cfc081f8a
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-11-02 00:36:52 +0600

    Create K.cpp

---
+++ class 08 - solve class/K.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define N ((int)1e6 + 5)
+#define MOD ((int)998244353 + 0)
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define MAXP ((int)1e3)
+#define endl "\n"
+
+using namespace std;
+
+int mark[2 + (MAXP/32)] , ord[MAXP+2];
+
+vector < int > prime;
+
+void sieve(int n)
+{
+    for(int i = 3 ; i*i<=n ; i += 2){
+        int idx = i>>5 , pos = i&31;
+        if((mark[idx] & (1<<pos)) == 0){
+            for(int j = i*i ; j<=n ; j += (i<<1)){
+                idx = j>>5 , pos = j&31;
+                mark[idx] |= 1<<pos;
+            }
+        }
+    }
+//    prime.push_back(2);
+    int tot = 1;
+    ord[2] = 1;
+    for(int i = 3 ; i<=n ; i += 2){
+        int idx = i>>5 , pos = i&31;
+        if((mark[idx] & (1<<pos)) == 0) ord[i] = ++tot;
+    }
+}
+
+
+int main()
+{
+    fastio;
+    int n;
+    cin>>n;
+    sieve(n);
+    vector < int > vec;
+    for(int i = 0 ; i<prime.size(); i++){
+        int p = prime[i];
+        int tmp = p;
+        while(tmp<=n){
+            vec.push_back(tmp);
+            tmp *= p;
+        }
+    }
+
+    cout<<vec.size()<<endl;
+    for(int i = 0 ; i<vec.size() ; i++) cout<<vec[i]<<" ";
+
+    return 0;
+}

1948303341c3db4af406c2c3ee6af13a92d8504d
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-11-16 23:03:39 +0600

    Create N.cpp

---
+++ class 12 - solve class/N.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define endl "\n"
+#define ll long long
+#define N ((int)(3e5 + 5))
+#define MOD ((int)(1e9 + 7))
+
+
+using namespace std;
+
+
+/*fast io
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+*/
+
+
+
+
+int main()
+{
+    fastio;
+    int n;
+    cin>>n;
+    if(n == 2) cout<<"-1\n";
+    else{
+        cout<<"6\n15\n";
+        ll val = 10;
+        for(int i = 3 ; i<=n ; i++){
+            cout<<val<<"\n";
+            val *= 2;
+        }
+    }
+    return 0;
+}

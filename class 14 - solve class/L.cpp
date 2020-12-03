e7f264f5cca23b16bb272c995147f36ad294da78
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-12-04 01:21:01 +0600

    Update L.cpp

--- class 14 - solve class/L.cpp
+++ class 14 - solve class/L.cpp
@@ -2,9 +2,9 @@
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
 #define endl "\n"
 #define ll long long
-#define N ((int)(2e5 + 5))
-#define MAX ((int)(2e9 + 5))
-#define MOD ((int)(7901 + 0))
+#define N ((int)2e5 + 5)
+#define MAX ((int)2e9 + 5)
+#define MOD ((int)7901 + 0)
 
 
 using namespace std;
@@ -23,6 +23,11 @@ int main()
     fastio;
     ll a , b , x , y;
     cin>>a>>b>>x>>y;
+
+    ll g = __gcd(x,y);
+    x /= g;
+    y /= g;
+
     ll lef = 0 , rig = MAX;
     while(lef < rig){
         ll mid = (lef + rig + 1)/2;
